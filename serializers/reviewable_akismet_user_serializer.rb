33aefd84a7e797adc2b7b438ef7091bfa660472e
Roman Rizzi <rizziromanalejandro@gmail.com>
2019-08-13 17:39:09 -0300

    FEATURE: Link to the user profile if possible when reviewing AkismetFlaggedUsers (#42)

--- serializers/reviewable_akismet_user_serializer.rb
+++ serializers/reviewable_akismet_user_serializer.rb
@@ -4,4 +4,10 @@ require_dependency 'reviewable_serializer'
 
 class ReviewableAkismetUserSerializer < ReviewableSerializer
   payload_attributes :username, :name, :email, :bio
+
+  attributes :user_deleted
+
+  def user_deleted
+    object.target.nil?
+  end
 end
