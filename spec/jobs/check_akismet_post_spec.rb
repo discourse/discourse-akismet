40835cef9f805de324a680c1798a6ef3c2311060
romanrizzi <rizziromanalejandro@gmail.com>
2019-11-04 10:54:29 -0300

    DEV: Add test to ensure that Jobs::CheckAkismetPost works

--- spec/jobs/check_akismet_post_spec.rb
+++ spec/jobs/check_akismet_post_spec.rb
@@ -6,9 +6,18 @@ RSpec.describe Jobs::CheckAkismetPost do
   before { SiteSetting.akismet_enabled = true }
 
   describe '#execute' do
+    let(:post) { Fabricate(:post) }
+
     it 'does not create a reviewable when a reviewable queued post already exists for that target' do
-      post = Fabricate(:post)
       ReviewableQueuedPost.needs_review!(target: post, created_by: Discourse.system_user)
+
+      subject.execute(post_id: post.id)
+
+      expect(ReviewableAkismetPost.count).to be_zero
+    end
+
+    it 'does not create a reviewable when the post is not spam' do
+      Akismet::Client.any_instance.stubs(:comment_check).returns(false)
 
       subject.execute(post_id: post.id)
 
