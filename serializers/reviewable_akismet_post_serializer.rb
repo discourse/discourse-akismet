89d46255e0eaf28294f28190be7cf54b1b6d26cc
Ted Johansson <ted@discourse.org>
2024-02-19 14:16:20 +0800

    DEV: Have Akismet flagged posts display the context question

--- serializers/reviewable_akismet_post_serializer.rb
+++ serializers/reviewable_akismet_post_serializer.rb
@@ -4,4 +4,8 @@ require_dependency "reviewable_serializer"
 
 class ReviewableAkismetPostSerializer < ReviewableSerializer
   payload_attributes :post_cooked, :external_error
+
+  def created_from_flag?
+    true
+  end
 end
